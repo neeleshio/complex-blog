81fa3e7afecdabfd62500a0c968cc19fa25a0428
neeleshio <neeleshiodev@gmail.com>
2024-04-15 12:34:29 +0530

    blog layout final

--- src/app/blogs/[slug]/index.tsx
+++ src/app/blogs/[slug]/index.tsx
@@ -8,21 +8,56 @@ import { Header } from '@/src/containers';
 import { Provider } from 'react-redux';
 import { store } from '@/src/store';
 import SectionHeader from '@/src/components/SectionHeader';
+import { FaWhatsapp } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+import { IoIosMail, IoIosLink } from 'react-icons/io';
 
 const customStyle = {
     fontSize: '1.4rem',
     borderRadius: 6,
     boxShadow: '0px 0px 43px -1px rgba(88, 86, 86, 0.75)',
-    margin: 40
+    margin: '4rem 0'
 };
 
 const components = {
     h1: ({ children }) => (
-        <h1 className="text-[#1d1d1f] font-semibold text-[4.8rem]" style={{ margin: 20 }}>
-            {children}
-        </h1>
+        <div className="mb-[2rem]">
+            <div className="mb-12">
+                <div className="text-[#6e6e73] text-[1.2rem] font-semibold">JAVASCRIPT</div>
+                <div className="text-[#6e6e73] text-[1.4rem] font-medium mt-[0.5rem]">
+                    04 March 2024
+                </div>
+            </div>
+            <h1 className="text-[#1d1d1f] font-semibold text-[4.8rem] leading-[50px]">
+                {children}
+            </h1>
+        </div>
     ),
-    p: ({ children }) => <p className="text-[1.7rem] text-[#1d1d1f]">{children}</p>,
+    blockquote: ({ children }) => (
+        <div className="mb-16">
+            <div className="text-[2.2rem] font-medium text-[#1d1d1f] leading-[28px]">
+                {children?.[1]?.['props']?.['children']}
+            </div>
+
+            <div>
+                <div className="mt-[3.2rem] blog-share">
+                    <button>
+                        <FaWhatsapp />
+                    </button>
+                    <button>
+                        <FaXTwitter />
+                    </button>
+                    <button>
+                        <IoIosMail />
+                    </button>
+                    <button>
+                        <IoIosLink />
+                    </button>
+                </div>
+            </div>
+        </div>
+    ),
+    p: ({ children }) => <p className="text-[1.7rem] text-[#1d1d1f] mb-6">{children}</p>,
     code: ({ children }) => (
         <SyntaxHighlighter
             language="javascript"
@@ -37,10 +72,13 @@ const components = {
             sizes="100vw"
             width={0}
             height={0}
-            style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+            style={{ width: '100%', height: 'auto', objectFit: 'cover', paddingBottom: '3rem' }}
             {...(props as ImageProps)}
         />
-    )
+    ),
+    li: ({ children }) => <li className="list-disc mx-16 mb-6">{children}</li>,
+    h2: ({ children }) => <h2 className="mb-10 mt-10 text-[#235bc1] text-[2.7rem]">{children}</h2>,
+    h3: ({ children }) => <h3 className="mb-6 text-[2rem]">{children}</h3>
 };
 
 export function CustomMDX(props) {
@@ -48,7 +86,7 @@ export function CustomMDX(props) {
         <Provider store={store}>
             <Header />
             <SectionHeader />
-            <div className="w-[98rem] m-auto">
+            <div className="w-[98rem] m-auto px-32 py-24">
                 <MDXRemote {...props} components={{ ...components, ...(props.components || {}) }} />
             </div>
         </Provider>
