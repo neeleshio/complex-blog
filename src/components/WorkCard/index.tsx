d4fd1152f2224eccf2d45f16c5437a7010a2b014
neeleshio <neeleshiodev@gmail.com>
2024-04-17 17:50:16 +0530

    responsive workcard

--- src/components/WorkCard/index.tsx
+++ src/components/WorkCard/index.tsx
@@ -12,14 +12,14 @@ type WorkCardProps = {
 
 const WorkCard = ({ company, designation, timeline, image: Img, latest }: WorkCardProps) => {
     return (
-        <div className="rounded-[18px] shadow-[2px_4px_12px_#0071e314] relative mr-8 my-8 hover:shadow-[2px_4px_16px_#0071e324] cursor-pointer custom-transform min-w-[40rem] h-[50rem]">
+        <div className="rounded-[18px] shadow-[2px_4px_12px_#0071e314] relative mr-8 my-8 hover:shadow-[2px_4px_16px_#0071e324] cursor-pointer custom-transform min-w-[40rem] h-[50rem] sm:min-w-[35rem] sm:h-[45rem]">
             {latest ? (
                 <div className="flex flex-col h-full justify-between rounded-[18px] bg-white overflow-hidden">
                     <div className="flex flex-col p-12">
                         <span className="text-[#6e6e73] uppercase text-[1.2rem] pb-5 font-semibold">
                             {designation}
                         </span>
-                        <span className="text-[2.5rem] text-[#1d1d1f] font-semibold">
+                        <span className="text-[2.5rem] text-[#1d1d1f] font-semibold sm:text-[2.1rem]">
                             {company}
                         </span>
                         <span className="pt-4 text-[1.3rem] text-[#1d1d1f] font-light">
@@ -36,7 +36,7 @@ const WorkCard = ({ company, designation, timeline, image: Img, latest }: WorkCa
                         <span className="text-[#f5f5f7] uppercase text-[1.2rem] pb-5 font-semibold">
                             {designation}
                         </span>
-                        <span className="text-[2.5rem] text-[#ffffff] font-semibold">
+                        <span className="text-[2.5rem] text-[#ffffff] font-semibold sm:text-[2.1rem]">
                             {company}
                         </span>
                         <span className="pt-4 text-[1.3rem] text-[#ffffff] font-light">
