d06541e9443893497f992c9fc517ef682370c63f
neeleshio <neeleshiodev@gmail.com>
2024-05-03 23:04:50 +0530

    waving hand

--- src/components/Name/index.tsx
+++ src/components/Name/index.tsx
@@ -4,7 +4,7 @@ const Name = () => {
     return (
         <div className="max-w-[61rem] leading-[4.5rem] md:leading-[4.3rem] sm:leading-[4rem]">
             <h1 className="inline text-[4rem] font-semibold md:text-[3.8rem] sm:text-[3.2rem]">
-                Hi 👋🏻, I'm Neelesh.
+                Hi <span className="wave">👋🏻</span>, I'm Neelesh.
             </h1>
             <span className="text-[4rem] text-cgrey font-semibold pl-4 md:text-[3.8rem] sm:text-[3.2rem]">
                 A Frontend Software Development Engineer.
